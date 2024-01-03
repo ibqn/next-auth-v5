afaab351609c0070df387e639d25315aaa60d5f6
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-03 10:54:46 +0100

    play with settings page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,11 +1,22 @@
-import { auth } from "@/auth"
+import { auth, signOut } from "@/auth"
+import { Button } from "@/components/ui/button"
 
 export default async function SettingsPage() {
   const session = await auth()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       settings
       <div>{JSON.stringify(session)}</div>
+      <form
+        action={async () => {
+          "use server"
+
+          await signOut()
+        }}
+      >
+        <Button type="submit">Sign Out</Button>
+      </form>
     </main>
   )
 }
