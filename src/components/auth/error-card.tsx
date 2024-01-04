9791d0957ba863123f5925f33b5bfacbf9acd9e3
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-04 11:20:27 +0100

    wip on error card

---
+++ src/components/auth/error-card.tsx
@@ -0,0 +1,5 @@
+type Props = {}
+
+export const ErrorCard = (props: Props) => {
+  return <div>ErrorCard</div>
+}
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,2 +1,4 @@
 export { SignInForm } from "./sign-in-form"
 export { SignUpForm } from "./sign-up-form"
+
+export { ErrorCard } from "./error-card"
