1518995a58df07b219daf3d34a4ad42fb10cf24c
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-04 11:19:41 +0100

    add user roles

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
+import { getUserById } from "@/utils/prisma"
+import { type UserRole } from "@prisma/client"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: UserRole
+    } & DefaultSession["user"]
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +19,57 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: "/auth/sign-in",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      })
+    },
+  },
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id)
+    //   if (!existingUser?.emailVerified) {
+    //     return false
+    //   }
+    //   return true
+    // },
+    async jwt({ token }) {
+      console.log("token >", { token })
+
+      if (!token.sub) {
+        return token
+      }
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) {
+        return token
+      }
+
+      token.role = existingUser.role
+
+      return token
+    },
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+
+      console.log("session >", { session })
+
+      return session
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
