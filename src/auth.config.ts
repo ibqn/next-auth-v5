05c64e2f44f320ac3db8bfc5886e17c8aa8e3894
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-04 13:16:16 +0100

    mention linking option

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -11,10 +11,12 @@ export default {
     GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      // allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
       async authorize(credentials) {
