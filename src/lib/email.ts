74f088566e585b2100f8180a5f3217e87dad1c34
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-05 13:57:04 +0100

    fix email link

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -3,12 +3,12 @@ import { Resend } from "resend"
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmationLink = `http://localhost:3000/auth/email-verification/token=${token}`
+  const confirmationLink = `http://localhost:3000/auth/email-verification?token=${token}`
 
   resend.emails.send({
     from: "onboarding@resend.dev",
     to: `${email}`,
     subject: "Confirm your email",
-    html: `<p>Please click this <a href="$${confirmationLink}">link</a> to confirm email.</p>`,
+    html: `<p>Please click this <a href="${confirmationLink}">link</a> to confirm email.</p>`,
   })
 }
