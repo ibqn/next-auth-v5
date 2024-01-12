96020c5315e0712f04d712588f828ab030877300
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-12 18:17:41 +0100

    extend sing in validator

--- src/lib/validators/sign-in.ts
+++ src/lib/validators/sign-in.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 export const signInValidator = z.object({
   email: z.string().email({ message: "Email is required" }),
   password: z.string().trim().min(1, { message: "Password is required" }),
+  code: z.string().optional(),
 })
 
 export type SignInPayload = z.infer<typeof signInValidator>
