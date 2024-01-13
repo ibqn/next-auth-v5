05fad848c939b6c660806f59eddf4be1aa4cf3e9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-13 22:02:06 +0100

    export auth comps

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -6,3 +6,6 @@ export { ErrorCard } from "./error-card"
 export { EmailVerificationForm } from "./email-verification-from"
 export { PasswordResetForm } from "./password-reset-form"
 export { NewPasswordForm } from "./new-password-form"
+
+export { UserButton } from "./user-button"
+export { SignOutButton } from "./sign-out-button"
