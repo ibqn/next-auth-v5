3849c06b20e00d689d22d6069ff5f612a74bf59e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-13 13:36:08 +0100

    add navbar comp

---
+++ src/components/navbar.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useMemo } from "react"
+import { Button } from "@/components/ui/button"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+import { UserButton } from "@/components/auth"
+
+type Props = {}
+
+export const Navbar = (props: Props) => {
+  const navLinks = useMemo(
+    () => [
+      { path: "/client", name: "Client" },
+      { path: "/server", name: "Server" },
+      { path: "/admin", name: "Admin" },
+      { path: "/settings", name: "Settings" },
+    ],
+    []
+  )
+
+  const pathname = usePathname()
+
+  return (
+    <nav className="flex w-[600px] items-center justify-between rounded-xl bg-secondary p-4 shadow-sm">
+      <div className="flex gap-x-2">
+        {navLinks.map(({ path, name }, index) => (
+          <Button
+            key={index}
+            asChild
+            size="sm"
+            variant={pathname === path ? "default" : "outline"}
+          >
+            <Link href={path}>{name}</Link>
+          </Button>
+        ))}
+      </div>
+
+      <UserButton />
+    </nav>
+  )
+}
