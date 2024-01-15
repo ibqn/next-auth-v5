23f5f96484892727a7ffde3317b20cb7f02483cd
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-15 21:02:59 +0100

    improve protected layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function ProtectedLayout({ children }: Props) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-y-10 bg-gradient-120 from-blue-5 to-lime-3">
+    <div className="from-indigo-6 to-cyan-3 flex min-h-screen flex-col items-center justify-center gap-y-10 bg-gradient-120">
       <Navbar />
       {children}
     </div>
