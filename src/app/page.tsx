55125040cffa8644d48a2feb473c6fb8646eacc7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-19 16:39:01 +0100

    rename home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { KeySquare } from "lucide-react"
 import Link from "next/link"
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center space-y-7 bg-gradient-120 from-indigo-6 to-crimson-5 p-24">
       <div className="flex flex-row items-center gap-x-2 text-white drop-shadow-md">
