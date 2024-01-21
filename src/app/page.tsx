5e25982f5e0cd37d2eca801bbb730958119dbcd7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-21 11:34:14 +0100

    adjust icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center space-y-7 bg-gradient-120 from-indigo-6 to-crimson-5 p-24">
       <div className="flex flex-row items-center gap-x-2 text-white drop-shadow-md">
-        <KeySquare size={40} />
+        <KeySquare size={58} className="mb-2" />
         <h1 className="text-6xl font-semibold text-white ">Auth</h1>
       </div>
       <p className="text-lg text-white">A simple authentication service</p>
