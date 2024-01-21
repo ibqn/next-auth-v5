6e0c5477464f72d9ce6b9597a4d4c02d4f134d50
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-21 11:05:53 +0100

    protect admin routes in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,37 @@
-import NextAuth from "next-auth"
-import authConfig from "@/auth.config"
+import { auth } from "@/auth"
 import {
   DEFAULT_SIGN_IN_REDIRECT,
+  adminRoutes,
   apiAuthPrefix,
   authRoutes,
   publicRoutes,
 } from "@/routes"
-
-const { auth } = NextAuth(authConfig)
+import { NextResponse } from "next/server"
 
 export default auth((request) => {
   const { nextUrl } = request
   const isLoggedIn = !!request.auth
+  const isAdmin = request.auth?.user?.role === "ADMIN"
 
-  // req.auth
+  console.log("role", request.auth?.user.role)
+
   console.log("route:", request.nextUrl.pathname)
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  const isAdminRoute = adminRoutes.includes(nextUrl.pathname)
 
   if (isApiAuthRoute) {
     return null
+  }
+
+  if (isAdminRoute) {
+    if (isAdmin) {
+      return null
+    }
+
+    return NextResponse.rewrite(new URL("/404", nextUrl))
   }
 
   if (isAuthRoute) {
