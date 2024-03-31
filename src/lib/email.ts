73bd4d01ea29ce526950dd43965bb66e37f342c7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-03-31 16:58:55 +0200

    Commented out email sending code and added console logs for debugging

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -6,21 +6,35 @@ const resend = new Resend(env.RESEND_API_KEY)
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationLink = `http://localhost:3000/auth/email-verification?token=${token}`
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: `${email}`,
-    subject: "Confirm your email",
-    html: `<p>Please click this <a href="${confirmationLink}">link</a> to confirm email.</p>`,
-  })
+  // resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: `${email}`,
+  //   subject: "Confirm your email",
+  //   html: `<p>Please click this <a href="${confirmationLink}">link</a> to confirm email.</p>`,
+  // })
+
+  console.log(
+    "sendVerificationEmail",
+    email,
+    "confirmationLink",
+    confirmationLink
+  )
 }
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const passwordResetLink = `http://localhost:3000/auth/new-password?token=${token}`
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: `${email}`,
-    subject: "Reset your password",
-    html: `<p>Please click this <a href="${passwordResetLink}">link</a> to set a new password.</p>`,
-  })
+  // resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: `${email}`,
+  //   subject: "Reset your password",
+  //   html: `<p>Please click this <a href="${passwordResetLink}">link</a> to set a new password.</p>`,
+  // })
+
+  console.log(
+    "sendPasswordResetEmail",
+    email,
+    "passwordResetLink",
+    passwordResetLink
+  )
 }
